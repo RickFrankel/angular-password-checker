94bbcdf8540ae64654ebe881cd30ccf001876013
Rick Frankel <rick.frankel@gmail.com>
2023-02-15 17:42:30 +1000

    Fix such that null values are ignored.

--- projects/password-checker-lib/src/lib/password-checker-lib.directive.ts
+++ projects/password-checker-lib/src/lib/password-checker-lib.directive.ts
@@ -2,7 +2,7 @@ import { Observable, of, timer } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS, ValidationErrors } from '@angular/forms';
 import { Directive, Inject, Input, Optional } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpBackend, HttpClient, HttpHeaders } from '@angular/common/http';
 import sha1 from 'crypto-js/sha1';
 import { Partial, PasswordCheckerConfig, PasswordCheckerConfigValue } from './password-checker.config';
 
@@ -25,8 +25,10 @@ export class PasswordCheckerLibDirective implements AsyncValidator {
   @Input() pwnedPasswordApiCallDebounceTime: number;
   @Input() pwnedPasswordValidator: boolean;
 
+  private http: HttpClient;
+
   constructor(
-    private http: HttpClient,
+    handler: HttpBackend,
     @Optional() @Inject(PasswordCheckerConfigValue) config: Partial<PasswordCheckerConfig>,
   ) {
 
@@ -35,6 +37,8 @@ export class PasswordCheckerLibDirective implements AsyncValidator {
       // conflict with @Optional()?
       config = {};
     }
+
+    this.http = new HttpClient(handler);
 
     this.pwnedPasswordApi =
       config.pwnedPasswordApi
@@ -48,44 +52,49 @@ export class PasswordCheckerLibDirective implements AsyncValidator {
   }
 
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
-    const pw = ''.concat(control.value);
-    const pwnedPasswordValidator = this.pwnedPasswordValidator;
-    const headers = new HttpHeaders({
-      'Add-Padding': 'true',
-    });
+    if (control.value) {
+      const pw = ''.concat(control.value);
+      const pwnedPasswordValidator = this.pwnedPasswordValidator;
+      const headers = new HttpHeaders({
+        'Add-Padding': 'true',
+      });
 
-    if (pwnedPasswordValidator) {
-      return timer(this.pwnedPasswordApiCallDebounceTime).pipe(
-        map(() => {
-          const pwSha1 = sha1(pw).toString().toUpperCase();
+      if (pwnedPasswordValidator && pw) {
+        return timer(this.pwnedPasswordApiCallDebounceTime).pipe(
+          map(() => {
+            const pwSha1 = sha1(pw).toString().toUpperCase();
 
-          return {
-            firstPart: pwSha1.substring(0, 5),
-            lastPart: pwSha1.substring(5),
-          };
-        }),
-        switchMap(
-          (hash) => this.http.get(
-            `${this.pwnedPasswordApi}${hash.firstPart}`, { headers: headers, responseType: 'text' }
-          ).pipe(
-            map(passwords => passwords.split(/[\r\n]+/)),
-            map(passwords => passwords.map((password) => {
-              const split = password.split(':');
+            return {
+              firstPart: pwSha1.substring(0, 5),
+              lastPart: pwSha1.substring(5),
+            };
+          }),
+          switchMap(
+            (hash) => this.http.get(
+              `${this.pwnedPasswordApi}${hash.firstPart}`, { headers: headers, responseType: 'text' }
+            ).pipe(
+              map(passwords => passwords.split(/[\r\n]+/)),
+              map(passwords => passwords.map((password) => {
+                const split = password.split(':');
 
-              return {
-                hash: split[0],
-                count: parseInt(split[1], 10),
-              };
-            }
-            )),
-            map(passwords => passwords.find(password => password.hash === hash.lastPart && password.count > 0)),  //0 count hashes are added by the padding.
+                return {
+                  hash: split[0],
+                  count: parseInt(split[1], 10),
+                };
+              }
+              )),
+              map(passwords => passwords.find(password => password.hash === hash.lastPart && password.count > 0)),  //0 count hashes are added by the padding.
+            ),
           ),
-        ),
-        map(password => password && password.count >= this.pwnedPasswordMinimumOccurrenceForError
-          ? { pwnedPasswordOccurrence: password.count }
-          : null),
-      );
-    } else {
+          map(password => password && password.count >= this.pwnedPasswordMinimumOccurrenceForError
+            ? { pwnedPasswordOccurrence: password.count }
+            : null),
+        );
+      } else {
+        return of(null);
+      }
+    }
+    else {
       return of(null);
     }
   }
